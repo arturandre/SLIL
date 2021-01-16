7b12fda337524b891262c45350a3e6151ef79784
Artur Andre <arturpossible@gmail.com>
2021-01-16 18:15:57 -0300

    Uncertain label

--- slil.py
+++ slil.py
@@ -39,12 +39,28 @@ img_col.append([sg.Text('Image name: -' + '-'*140, key="txt_img_name")])
 
 labels_col = []
 
+unlabeled_code = '-10'
+positive_label = '1'
+negative_label = '-1'
+uncertain_label = '0'
+
+pressedNum = None
+waiting_decision = False
+
 summary_manager = SummaryManager()
+labels_header = [
+    sg.Text('-1   '),
+    sg.Text('0   '),
+    sg.Text('1'),
+    ]
 
 for i, label in enumerate(summary_manager.labels):
     labels_col.append([
-        sg.Checkbox(label, enable_events=True, key=f'label_{i}'),
-        sg.Text('*', key=f'unlabeled_{i}')
+        sg.Radio('', f"label{i}", enable_events=True, key=f'label_n{i}'),
+        sg.Radio('', f"label{i}", enable_events=True, key=f'label_u{i}'),
+        sg.Radio('', f"label{i}", enable_events=True, key=f'label_p{i}'),
+        sg.Text(label),
+        sg.Text('*', key=f'unlabeled_{i}'),
         ])
 labels_col.append([sg.Button('Save')])
 labels_col.append([sg.Button('<- Previous'), sg.Button('Next ->')])
@@ -56,9 +72,11 @@ labels_col.append([sg.Text('Image index: 000000/000000', key="txt_img_index")])
 labels_col.append([sg.Text('Check on GSV:')])
 labels_col.append([sg.Button('Pano'), sg.Button('Coordinates')])
 
+labels_col_header = [labels_header] + labels_col
+
 # All the stuff inside your window.
 layout = [[sg.Menu(menu_def, tearoff=True, key='menu')],
-          [sg.Column(img_col), sg.Column(labels_col)],
+          [sg.Column(img_col), sg.Column(labels_col_header)],
           ]
 
 # Create the Window
@@ -107,20 +125,20 @@ def get_next_unlabeled(backwards=False, ignore_unlabeled=False, ignore_exported=
         # if not backwards:
         next_unlabeled_ones = summary_manager.current_labelgui_summary.iloc[
                               current_sample_index::step][
-            summary_manager.current_labelgui_summary.iloc[current_sample_index::step][last_col] == '-1']
+            summary_manager.current_labelgui_summary.iloc[current_sample_index::step][last_col] == unlabeled_code]
         # else:
         #    next_unlabeled_ones = summary_manager.current_labelgui_summary.iloc[
         #                          :current_sample_index:step][
-        #        summary_manager.current_labelgui_summary.iloc[:current_sample_index:step][last_col] == '-1']
+        #        summary_manager.current_labelgui_summary.iloc[:current_sample_index:step][last_col] == unlabeled_code]
         if len(next_unlabeled_ones) == 0:
             # if not backwards:
             next_unlabeled_ones = summary_manager.current_labelgui_summary.iloc[
                                   :current_sample_index:step][
-                summary_manager.current_labelgui_summary.iloc[:current_sample_index:step][last_col] == '-1']
+                summary_manager.current_labelgui_summary.iloc[:current_sample_index:step][last_col] == unlabeled_code]
             # else:
             #    next_unlabeled_ones = summary_manager.current_labelgui_summary.iloc[
             #                          current_sample_index::step][
-            #        summary_manager.current_labelgui_summary.iloc[current_sample_index::step][last_col] == '-1']
+            #        summary_manager.current_labelgui_summary.iloc[current_sample_index::step][last_col] == unlabeled_code]
         if len(next_unlabeled_ones) > 0:
             current_sample_index = summary_manager.current_labelgui_summary.index.get_loc(next_unlabeled_ones.index[0])
             load_sample(current_sample_index)
@@ -140,7 +158,7 @@ def decrease_sample_index():
                 break
             next_sample = summary_manager.current_labelgui_summary.iloc[current_sample_index]
             if ignore_unlabeled:
-                if not next_sample[summary_manager.labels[0]] in (-1, '-1'):
+                if not next_sample[summary_manager.labels[0]] in (int(unlabeled_code), unlabeled_code):
                     continue
             if ignore_exported:
                 if next_sample['status'] == 'exported':
@@ -167,7 +185,7 @@ def increase_sample_index():
                 break
             next_sample = summary_manager.current_labelgui_summary.iloc[current_sample_index]
             if ignore_unlabeled:
-                if not next_sample[summary_manager.labels[0]] in (-1, '-1'):
+                if not next_sample[summary_manager.labels[0]] in (int(unlabeled_code), unlabeled_code):
                     continue
             if ignore_exported:
                 if next_sample['status'] == 'exported':
@@ -184,11 +202,22 @@ def set_sample_labels(index):
     if summary_manager.is_summary_loaded():
         current_labels = summary_manager.current_labelgui_summary.iloc[index][-len(summary_manager.labels):]
         for current_label in enumerate(current_labels):
-            # Checkbox
-            labels_col[current_label[0]][0].update(current_label[1] in [1, '1'])
+            # Radio buttons
+            line_idx = current_label[0]
+            label_line = labels_col[line_idx]
+            label = int(current_label[1]) #-10, -1, 0, 1
+            if label != -10:
+                decision_idx = label +1 # 0, 1, 2
+                label_line[decision_idx].update(True)
+            else:
+                # Hack to reset radio buttons
+                # https://github.com/PySimpleGUI/PySimpleGUI/issues/1350#issuecomment-487114267
+                label_line[0].TKIntVar.set(0)
+                #for i in range(3):
+                #    label_line[i].update(False)
             # Text (*)
-            labels_col[current_label[0]][1]\
-                .update('*' if current_label[1] in [-1, '-1'] else '')
+            labels_col[line_idx][-1]\
+                .update('*' if current_label[1] in [int(unlabeled_code), unlabeled_code] else '')
 
 
 def load_sample(index):
@@ -231,25 +260,42 @@ def load_sample(index):
     pass
 
 def save_in_memory():
-    sel_labels = \
-        [cb[0].get() for cb in labels_col[:len(summary_manager.labels)]]
+    sel_labels = []
+    for rb_line in labels_col[:len(summary_manager.labels)]:
+        if rb_line[0].get(): #-1
+            sel_labels.append(-1)
+        elif rb_line[1].get(): #0
+            sel_labels.append(0)
+        elif rb_line[2].get(): #1
+            sel_labels.append(1)
+        else:  #unlabeled
+            sel_labels.append(-10)
     summary_manager.\
         save_sample_labels(current_sample_index, sel_labels)
 
-def check_label_checkbox(label_index, mouse_click=False):
-    sel_checkbox = labels_col[label_index][0]
-    checked = bool(sel_checkbox.get())
-    if mouse_click: checked = not checked
-    sel_checkbox.update(not checked)
+def check_label_checkbox(label_index, label_decision, mouse_click=False):
+    if label_decision not in ['n', 'u', 'p']:
+        raise Exception('Unrecognized decision: {label_decision}, expected: [n, u, p].')
+    sel_radio_line = labels_col[label_index]
+    label_decision_idx = ['n', 'u', 'p'].index(label_decision) #0, 1, 2
+    label_decision = label_decision_idx -1 # -1, 0, 1
+    sel_radio = sel_radio_line[label_decision_idx]
+    sel_radio.update(True)
+
+    #checked = bool(sel_checkbox.get())
+    #if mouse_click: checked = not checked
+    #sel_checkbox.update(not checked)
     # Checking dependencies
-    sel_label = sel_checkbox.Text
-    if not checked: # Just checked the CheckBox right now
+    #sel_label = sel_checkbox.Text
+    sel_label = sel_radio_line[-2].DisplayText
+    if label_decision == 1: # 
         if sel_label in summary_manager.label_dependencies.keys():
             deps = summary_manager.label_dependencies[sel_label]
             for i in range(len(summary_manager.labels)):
-                aux_checkbox = labels_col[i][0]
-                if aux_checkbox.Text in deps:
-                    aux_checkbox.update(True)
+                aux_radio_line = labels_col[i]
+                aux_radio = aux_radio_line[2]
+                if aux_radio_line[-2].DisplayText in deps:
+                    aux_radio.update(True)
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
@@ -381,19 +427,27 @@ while True:
                 f"/data=!3m7!1e1!3m5!1s!2e0!5s20140101T000000!7i13312!8i6656"
             ))
             webbrowser.open_new_tab(googleaddress)
-        elif re.match(r'^label_\d+$', event):
-            checkbox_label = re.match(r'^label_(\d+)$', event).groups()[0]
-            checkbox_label = int(checkbox_label)
-            check_label_checkbox(checkbox_label, mouse_click=True)
+        elif re.match(r'^label_[nup]\d+$', event):
+            groups = re.match(r'^label_([nup])(\d+)$', event).groups()
+            decision = groups[0]
+            label = groups[1]
+            label = int(label)
+            check_label_checkbox(label, decision, mouse_click=True)
         elif len(event) == 0: # keyboard events
             if len(window.user_bind_event.char) == 1:  # numbers/letters/enter keys
                 if window.user_bind_event.char == '\r':  # Enter keys
                     save_in_memory()
                     continue
-                elif window.user_bind_event.char.isdigit(): # Number keys
+                elif window.user_bind_event.char.isdigit():  # Number keys
+                    waiting_decision = True
                     pressedNum = int(window.user_bind_event.char)
+                elif waiting_decision and (window.user_bind_event.char in ['a', 's', 'd']):
+                    decision = ['a', 's', 'd'].index(window.user_bind_event.char)
+                    decision = ['n', 'u', 'p'][decision]
                     if pressedNum < len(summary_manager.labels):
-                        check_label_checkbox(pressedNum)
+                        check_label_checkbox(pressedNum, decision)
+                if not window.user_bind_event.char.isdigit():  # Number keys
+                    waiting_decision = False
             elif window.user_bind_event.keysym == 'Left': # Enter keys:
                 decrease_sample_index()
             elif window.user_bind_event.keysym == 'Right': # Enter keys:
